2377ad25482f6210a2032d37e567bcc224b66aea
Jon Finger <jaefinger@gmail.com>
2020-07-12 22:31:20 -0500

    adjusted relative filepath

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -9,13 +9,13 @@ from web_app.routes.home_routes import home_routes
 from web_app.routes.student_routes import student_routes
 
 load_dotenv()
-DATABASE_URI = os.getenv("DATABASE_URI")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 
 def create_app():
     app = Flask(__name__)
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
     migrate.init_app(app, db)
