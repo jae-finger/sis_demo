a0baf7f17f5ffcef94587990c121dc339180d970
Jon Finger <jaefinger@gmail.com>
2020-07-13 16:44:11 -0500

    adjusted sqlite init

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -9,13 +9,15 @@ from web_app.routes.home_routes import home_routes
 from web_app.routes.student_routes import student_routes
 
 load_dotenv()
-DATABASE_URI = os.getenv("DATABASE_URI")
+DATABASE_URL = os.getenv("DATABASE_URL")
+SECRET_KEY = os.getenv("SECRET_KEY", default="super secret")
 
 
 def create_app():
     app = Flask(__name__)
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///sis_vis_dev.db'
+    app.config["SECRET_KEY"] = SECRET_KEY
+    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
     migrate.init_app(app, db)
