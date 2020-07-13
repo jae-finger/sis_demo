640b82153ff441ddc37d36a285a169fb4746435c
Jon Finger <jaefinger@gmail.com>
2020-07-12 22:16:44 -0500

    added seed and clear db

--- web_app/models.py
+++ web_app/models.py
@@ -9,4 +9,24 @@ migrate = Migrate()
 
 class Student(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(128))
+    student_name = db.Column(db.String)
+
+def parse_records(database_records):
+    """
+    A helper method for converting a list of database record objects into a list of dictionaries, so they can be returned as JSON
+    Param: database_records (a list of db.Model instances)
+    Example: parse_records(User.query.all())
+    Returns: a list of dictionaries, each corresponding to a record, like...
+        [
+            {"id": 1, "title": "Book 1"},
+            {"id": 2, "title": "Book 2"},
+            {"id": 3, "title": "Book 3"},
+        ]
+    """
+    parsed_records = []
+    for record in database_records:
+        print(record)
+        parsed_record = record.__dict__
+        del parsed_record["_sa_instance_state"]
+        parsed_records.append(parsed_record)
+    return parsed_records
--- web_app/routes/student_routes.py
+++ web_app/routes/student_routes.py
@@ -1,9 +1,37 @@
 # web_app/routes/student_routes.py
 
-from flask import Blueprint, render_template
+from web_app.models import db, Student, parse_records
+from flask import Blueprint, render_template, jsonify
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 student_routes = Blueprint("student_routes", __name__)
 
+
 @student_routes.route("/students")
 def student_list():
-    return render_template("students.html", message="Here is the list of students in D3000:")
+    students = Student.query.all()
+    return render_template("students.html", students=students)
+
+
+@student_routes.route("/seedx")
+def seed_students():
+    db.session.add(Student(student_name="Jon"))
+    db.session.add(Student(student_name="Tony"))
+    db.session.add(Student(student_name="Sandy"))
+    db.session.add(Student(student_name="Ryan"))
+    db.session.add(Student(student_name="Mike"))
+    db.session.add(Student(student_name="Hugo"))
+    db.session.add(Student(student_name="Jesse"))
+    db.session.add(Student(student_name="Paul"))
+    db.session.add(Student(student_name="Kathi"))
+    db.session.commit()
+    students = Student.query.all()
+    return render_template("students.html", students=students)
+
+@student_routes.route("/clearx")
+def clear_students():
+    Student.query.delete()
+    db.session.commit()
+    students = Student.query.all()
+    return render_template("students.html", students=students)
