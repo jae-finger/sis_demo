d96cc1b86f80e2ce2f63d191e875ea1b28cb4e1f
Jon Finger <jaefinger@gmail.com>
2020-07-12 22:42:09 -0500

    fixing db

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -9,13 +9,13 @@ from web_app.routes.home_routes import home_routes
 from web_app.routes.student_routes import student_routes
 
 load_dotenv()
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URI = os.getenv("DATABASE_URI")
 
 
 def create_app():
     app = Flask(__name__)
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
+    app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///sis_vis_dev.db'
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
     migrate.init_app(app, db)
