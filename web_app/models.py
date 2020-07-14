62c853dd10606703c5b2a8080d889a40de231f38
Jon Finger <jaefinger@gmail.com>
2020-07-14 00:07:51 -0500

    stable dev 07 13

--- web_app/models.py
+++ web_app/models.py
@@ -10,7 +10,7 @@ migrate = Migrate()
 class Student(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     student_name = db.Column(db.String)
-    scores = db.relationship('Score')
+    # scores = db.relationship('Score')
 
 # class Course(db.Model):
 #     id = db.Column(db.Integer, primary_key=True)
@@ -23,24 +23,24 @@ class Student(db.Model):
 #     class_id = db.Column(db.Integer, db.ForeignKey('course.id'))
 
 class Course(db.Model):
-    __tablename__ = 'course'
+    __tablename__ = 'courses'
     id = db.Column(db.Integer, primary_key=True)
     course_name = db.Column(db.String)
-    teacher = db.relationship('Teacher', uselist=False, back_populates='course')
-    scores = db.relationship('Score')
+    teacher_id = db.Column(db.Integer, db.ForeignKey('teachers.id'))
+    teachers = db.relationship('Teacher', uselist=False, back_populates='courses')
+    # scores = db.relationship('Score')
 
 class Teacher(db.Model):
-    __tablename__ = 'teacher'
+    __tablename__ = 'teachers'
     id = db.Column(db.Integer, primary_key=True)
     teacher_name = db.Column(db.String)
-    course_id = db.Column(db.Integer, db.ForeignKey('course.id'))
-    course = db.relationship('Course', back_populates='teacher')
+    courses = db.relationship('Course', back_populates='teachers')
 
 class Score(db.Model):
-    __tablename__ = 'score'
+    __tablename__ = 'scores'
     id = db.Column(db.Integer, primary_key=True)
-    class_id = db.Column(db.Integer, db.ForeignKey('course.id'))
-    student_id = db.Column(db.Integer, db.ForeignKey('student.id'))
+    class_id = db.Column(db.Integer, db.ForeignKey('courses.id'))
+    # student_id = db.Column(db.Integer, db.ForeignKey('student.student_id'))
     homework = db.Column(db.String)
     score = db.Column(db.Integer)
 
