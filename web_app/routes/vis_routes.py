2e96710fb2345d22946e8c77ebdac1515e1e2ce2
Jon Finger <jaefinger@gmail.com>
2020-07-15 00:01:40 -0500

    testing vis

--- web_app/routes/vis_routes.py
+++ web_app/routes/vis_routes.py
@@ -15,14 +15,14 @@ DEV_DB_FILEPATH = os.path.join(os.path.dirname(__file__), "..", "sis_vis_dev.db"
 
 @vis_routes.route("/visuals")
 def visuals():
-    # #FilePath
-    # dev_conn = sqlite3.connect(DEV_DB_FILEPATH)
-    # # print("Connection:", dev_conn)
-    # cursor1 = dev_conn.cursor()
-    # # print("CURSOR:", cursor1)
-    # gradebook_query = "SELECT t.student_name, s.assignment_name, s.score, c.course_name, r.teacher_name FROM scores s LEFT JOIN student t ON s.student_id = t.id LEFT JOIN courses c ON s.class_id = c.id LEFT JOIN teachers r ON c.teacher_id = r.id;"
-    # gradebook_result = cursor1.execute(gradebook_query).fetchall()
-    # colz = ['Student', 'Assignment', 'Score', 'Class', 'Teacher']
-    # gradebook_df = pd.DataFrame(gradebook_result, columns=colz)
-    return render_template("index.html")
-    # return render_template("visuals.html", gradebook=[gradebook_df.to_html(classes='data')], header="true")
+    #FilePath
+    dev_conn = sqlite3.connect(DEV_DB_FILEPATH)
+    # print("Connection:", dev_conn)
+    cursor1 = dev_conn.cursor()
+    # print("CURSOR:", cursor1)
+    gradebook_query = "SELECT t.student_name, s.assignment_name, s.score, c.course_name, r.teacher_name FROM scores s LEFT JOIN student t ON s.student_id = t.id LEFT JOIN courses c ON s.class_id = c.id LEFT JOIN teachers r ON c.teacher_id = r.id;"
+    gradebook_result = cursor1.execute(gradebook_query).fetchall()
+    colz = ['Student', 'Assignment', 'Score', 'Class', 'Teacher']
+    gradebook_df = pd.DataFrame(gradebook_result, columns=colz)
+    # return render_template("index.html")
+    return render_template("visuals.html", gradebook=[gradebook_df.to_html(classes='data')], header="true")
