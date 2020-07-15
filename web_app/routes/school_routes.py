d6b82a043d87d3b4f9d1c6712b18a48f81b2c747
Jon Finger <jaefinger@gmail.com>
2020-07-14 22:34:30 -0500

    fixed syntax

--- web_app/routes/school_routes.py
+++ web_app/routes/school_routes.py
@@ -90,7 +90,6 @@ def clear_db():
     def clear_data(session):
         meta = db.metadata
         for table in reversed(meta.sorted_tables):
-            print 'Clear table %s' % table
             session.execute(table.delete())
         session.commit()
     clear_data(db.session)
